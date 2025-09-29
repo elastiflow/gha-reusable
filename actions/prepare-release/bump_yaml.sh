63afb96cf9e278bb0f8ac2c5c47556ea9bf5e529
Mack (Maksym Iv) <146473578+maksym-iv-ef@users.noreply.github.com>
2025-09-29 19:17:31 +0300

    feat: Support multiple YAML keys update (#14)
    * feat: Support multiple YAML keys update
    
    Support for multiple YAML keys update in the prepare-release action
    
    * Update actions/prepare-release/bump_yaml.sh
    
    Co-authored-by: Keli (Madison) Grubb <keligrubb324@gmail.com>
    
    ---------
    
    Co-authored-by: Keli (Madison) Grubb <keligrubb324@gmail.com>

--- actions/prepare-release/bump_yaml.sh
+++ actions/prepare-release/bump_yaml.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
-set -x
 
 yaml_path=$1
 yaml_key=$2
@@ -10,14 +9,22 @@ if [ -z "${yaml_path}" ]; then echo "'yaml_path (\$1)' is not set, exiting"; exi
 if [ -z "${yaml_key}" ]; then echo "'yaml_key (\$2)' is not set, exiting"; exit 1; fi
 if [ -z "${new_release_version}" ]; then echo "'new_release_version (\$3)' is not set, exiting"; exit 1; fi
 
-yq "${yaml_key} = \"${new_release_version}\"" ${yaml_path} > /tmp/new.yaml
-if grep -q ${new_release_version} /tmp/new.yaml; then
-    echo 'Version update succeed, new:'
-    grep ${new_release_version} /tmp/new.yaml
-else
-    echo 'Version update failed, exiting'
-    exit 1
-fi
+keys=$(echo $yaml_key | tr ',' '\n')
 
-diff -U0 -w -b --ignore-blank-lines ${yaml_path} /tmp/new.yaml > /tmp/version.diff || true
-patch ${yaml_path} < /tmp/version.diff
+update_yaml_key() {
+    yq "${1} = \"${new_release_version}\"" ${yaml_path} > /tmp/new.yaml
+    if grep -q ${new_release_version} /tmp/new.yaml; then
+        echo 'Version update succeeded, new:'
+        grep ${new_release_version} /tmp/new.yaml
+    else
+        echo 'Version update failed, exiting'
+        exit 1
+    fi
+
+    diff -U0 -w -b --ignore-blank-lines ${yaml_path} /tmp/new.yaml > /tmp/version.diff || true
+    patch ${yaml_path} < /tmp/version.diff
+}
+
+for k in ${keys}; do
+    update_yaml_key ${k}
+done
