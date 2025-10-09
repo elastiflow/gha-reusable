27fc03d555c1cd449fda5a4cc0abc31e595d5232
Mack (Maksym Iv) <146473578+maksym-iv-ef@users.noreply.github.com>
2025-10-09 19:01:43 +0300

    feat: Replace "semantic-releases" with "git-cliff" (#17)
    * feat: Replace "semantic-releases" with "git-cliff"
    
    * chore: Examples
    
    * chore: Markdownlint
    
    * chore: Cleanup
    
    * fix: Consider only "this" branch
    
    * Update actions/prepare-release/action.yml
    
    Co-authored-by: Keli (Madison) Grubb <keligrubb324@gmail.com>
    
    * Update actions/prepare-release/action.yml
    
    Co-authored-by: Keli (Madison) Grubb <keligrubb324@gmail.com>
    
    ---------
    
    Co-authored-by: Keli (Madison) Grubb <keligrubb324@gmail.com>

--- actions/prepare-release/bump_yaml.sh
+++ actions/prepare-release/bump_yaml.sh
@@ -12,19 +12,19 @@ if [ -z "${new_release_version}" ]; then echo "'new_release_version (\$3)' is no
 keys=$(echo $yaml_key | tr ',' '\n')
 
 update_yaml_key() {
-    yq "${1} = \"${new_release_version}\"" ${yaml_path} > /tmp/new.yaml
-    if grep -q ${new_release_version} /tmp/new.yaml; then
-        echo 'Version update succeeded, new:'
-        grep ${new_release_version} /tmp/new.yaml
-    else
-        echo 'Version update failed, exiting'
-        exit 1
-    fi
+	yq "${1} = \"${new_release_version}\"" ${yaml_path} > /tmp/new.yaml
+	if grep -q "${new_release_version}" /tmp/new.yaml; then
+		echo 'Version update succeeded, new:'
+		grep "${new_release_version}" /tmp/new.yaml
+	else
+		echo 'Version update failed, exiting'
+		exit 1
+	fi
 
-    diff -U0 -w -b --ignore-blank-lines ${yaml_path} /tmp/new.yaml > /tmp/version.diff || true
-    patch ${yaml_path} < /tmp/version.diff
+	diff -U0 -w -b --ignore-blank-lines ${yaml_path} /tmp/new.yaml > /tmp/version.diff || true
+	patch ${yaml_path} < /tmp/version.diff
 }
 
 for k in ${keys}; do
-    update_yaml_key ${k}
+	update_yaml_key ${k}
 done
